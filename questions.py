96f7174c0457e664e4212299b839c900199f6f24
aripakaalekya-hue <aripakaalekya@gmail.com>
2026-07-03 15:03:24 +0530

    Add files via upload

---
+++ questions.py
@@ -0,0 +1,17 @@
+questions = [
+    {
+        "question": "What is the capital of India?",
+        "options": ["Delhi", "Mumbai", "Chennai", "Kolkata"],
+        "answer": "Delhi"
+    },
+    {
+        "question": "Which language is used with Flask?",
+        "options": ["Java", "Python", "C++", "PHP"],
+        "answer": "Python"
+    },
+    {
+        "question": "2 + 3 = ?",
+        "options": ["4", "5", "6", "7"],
+        "answer": "5"
+    }
+]
